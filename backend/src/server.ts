d36b8eab2c8ccf48a77413ca5530c6eba22b0c01
nevr001 <mariodev7@gmail.com>
2021-02-22 15:18:48 -0300

    fix: Response content main route

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,7 +3,7 @@ import express from 'express'
 const app = express()
 
 app.get('/', (req, res) => {
-    res.json('Hello, World!')
+    res.json({ message: 'Hello, World!' })
 })
 
 app.listen(3000, () => console.log('Server\'s running'))
