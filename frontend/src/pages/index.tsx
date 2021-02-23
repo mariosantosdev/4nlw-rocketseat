c6da068fecc89dbb037fbaf5f3f70ce8ced31234
nevr001 <mariodev7@gmail.com>
2021-02-23 15:43:56 -0300

    Revert "feat: Created Profile Component"
    This reverts commit 3709c34ef74063acf2a96a53e81a21c57443bf0f.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,20 +1,10 @@
 import ExperienceBar from '../components/ExperienceBar'
-import Profile from '../components/Profile'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <ExperienceBar />
-
-      <section>
-        <div>
-          <Profile />
-        </div>
-        <div>
-
-        </div>
-      </section>
     </div>
   )
 }
