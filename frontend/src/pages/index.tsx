4ddfa3c240161b60663bcaa85a7a341178ffff96
nevr001 <mariodev7@gmail.com>
2021-02-23 16:07:07 -0300

    refactor: Change title page and added favicon

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 import CompletedChallenge from '../components/CompletedChallenge'
 import ExperienceBar from '../components/ExperienceBar'
 import Profile from '../components/Profile'
@@ -7,6 +9,9 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Início - Move.it</title>
+      </Head>
       <ExperienceBar />
 
       <section>
