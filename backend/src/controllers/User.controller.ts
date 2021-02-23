6138b31412abc8a2cb73ddcc7ca2d19af3c3b410
nevr001 <mariodev7@gmail.com>
2021-02-23 18:20:04 -0300

    feat: Created routes and controllers

---
+++ backend/src/controllers/User.controller.ts
@@ -0,0 +1,11 @@
+import { Request, Response } from 'express'
+
+class UserController {
+    async create(req: Request, res: Response) {
+        const body = req.body
+
+        return res.json(body)
+    }
+}
+
+export { UserController }
