b22a31430d4e1a51a19d7bfa4a56372fc05b332c
nevr001 <mariodev7@gmail.com>
2021-02-26 10:59:19 -0300

    feat: Using modal level up

--- frontend/src/contexts/ChallengeContext.tsx
+++ frontend/src/contexts/ChallengeContext.tsx
@@ -1,114 +1,124 @@
 import { createContext, ReactNode, useEffect, useState } from "react";
 import Cookies from 'js-cookie'
 import challenges from "../../challenges.json";
+import LevelUpModal from "../components/LevelUpModal";
 
 export const ChallengesContext = createContext({} as ChallengesContextData);
 
 interface ChallengeProps {
-	type: string;
-	description: string;
-	amount: number;
+    type: string;
+    description: string;
+    amount: number;
 }
 
 export interface ChallengesContextData {
-	level: number;
-	xpToNextLevel: number;
-	currentXp: number;
-	challengesCompleted: number;
-	activeChallenge: ChallengeProps | null;
-	actions: {
-		levelUp: () => void;
-		startNewChallenge: () => void;
-		resetChallenge: () => void;
-		completeChallenge: () => void;
-	};
+    level: number;
+    xpToNextLevel: number;
+    currentXp: number;
+    challengesCompleted: number;
+    activeChallenge: ChallengeProps | null;
+    actions: {
+        levelUp: () => void;
+        startNewChallenge: () => void;
+        resetChallenge: () => void;
+        completeChallenge: () => void;
+        handleLevelModal: () => void;
+    };
 }
 
 interface ChallengesProviderProps {
-	children?: ReactNode;
-	level: number
-	currentXp: number
-	challengesCompleted: number
+    children?: ReactNode;
+    level: number
+    currentXp: number
+    challengesCompleted: number
 }
 
 export function ChallengesProvider({ children, ...rest }: ChallengesProviderProps) {
-	const [level, setLevel] = useState(rest.level ?? 1);
-	const [currentXp, setCurrentXp] = useState(rest.currentXp ?? 0);
-	const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
+    const [isShowLevelModal, setIsShowLevelModal] = useState(false)
+    const [level, setLevel] = useState(rest.level ?? 1);
+    const [currentXp, setCurrentXp] = useState(rest.currentXp ?? 0);
+    const [challengesCompleted, setChallengesCompleted] = useState(rest.challengesCompleted ?? 0);
 
-	const [activeChallenge, setActiveChallenge] = useState<ChallengeProps | null>(
-		null
-	);
+    const [activeChallenge, setActiveChallenge] = useState<ChallengeProps | null>(
+        null
+    );
 
-	const xpToNextLevel = Math.pow((level + 1) * 4, 2);
+    const xpToNextLevel = Math.pow((level + 1) * 4, 2);
 
-	useEffect(() => {
-		Notification.requestPermission();
-	}, []);
+    useEffect(() => {
+        Notification.requestPermission();
+    }, []);
 
-	useEffect(() => {
-		Cookies.set('level', String(level))
-		Cookies.set('currentXp', String(currentXp))
-		Cookies.set('challengesCompleted', String(challengesCompleted))
-	}, [level, currentXp, challengesCompleted])
+    useEffect(() => {
+        Cookies.set('level', String(level))
+        Cookies.set('currentXp', String(currentXp))
+        Cookies.set('challengesCompleted', String(challengesCompleted))
+    }, [level, currentXp, challengesCompleted])
 
-	function startNewChallenge() {
-		const randomChallenge = Math.floor(Math.random() * challenges.length);
-		const challenge = challenges[randomChallenge];
+    function startNewChallenge() {
+        const randomChallenge = Math.floor(Math.random() * challenges.length);
+        const challenge = challenges[randomChallenge];
 
-		setActiveChallenge(challenge);
+        setActiveChallenge(challenge);
 
-		new Audio("/notification.mp3").play();
+        new Audio("/notification.mp3").play();
 
-		if (Notification.permission === "granted") {
-			new Notification("Novo desafio 🚀", {
-				body: `Valendo ${challenge.amount}xp!`,
-				icon: "/favicon.png",
-			});
-		}
-	}
+        if (Notification.permission === "granted") {
+            new Notification("Novo desafio 🚀", {
+                body: `Valendo ${challenge.amount}xp!`,
+                icon: "/favicon.png",
+            });
+        }
+    }
 
-	function resetChallenge() {
-		setActiveChallenge(null);
-	}
+    function resetChallenge() {
+        setActiveChallenge(null);
+    }
 
-	function completeChallenge() {
-		if (!activeChallenge) return;
+    function completeChallenge() {
+        if (!activeChallenge) return;
 
-		const { amount } = activeChallenge;
-		let finalXp = currentXp + amount;
+        const { amount } = activeChallenge;
+        let finalXp = currentXp + amount;
 
-		if (finalXp >= xpToNextLevel) {
-			finalXp = finalXp - xpToNextLevel;
-			levelUp();
-		}
+        if (finalXp >= xpToNextLevel) {
+            finalXp = finalXp - xpToNextLevel;
+            levelUp();
+        }
 
-		setCurrentXp(finalXp);
-		setActiveChallenge(null);
-		setChallengesCompleted(challengesCompleted + 1);
-	}
+        setCurrentXp(finalXp);
+        setActiveChallenge(null);
+        setChallengesCompleted(challengesCompleted + 1);
+    }
 
-	function levelUp() {
-		setLevel(level + 1);
-	}
+    function levelUp() {
+        setLevel(level + 1);
+        handleLevelModal()
+    }
 
-	return (
-		<ChallengesContext.Provider
-			value={{
-				level,
-				xpToNextLevel,
-				currentXp,
-				challengesCompleted,
-				activeChallenge,
-				actions: {
-					levelUp,
-					startNewChallenge,
-					resetChallenge,
-					completeChallenge,
-				},
-			}}
-		>
-			{children}
-		</ChallengesContext.Provider>
-	);
+    function handleLevelModal() {
+        setIsShowLevelModal(!isShowLevelModal)
+    }
+
+    return (
+        <ChallengesContext.Provider
+            value={{
+                level,
+                xpToNextLevel,
+                currentXp,
+                challengesCompleted,
+                activeChallenge,
+                actions: {
+                    levelUp,
+                    startNewChallenge,
+                    resetChallenge,
+                    completeChallenge,
+                    handleLevelModal
+                },
+            }}
+        >
+            {isShowLevelModal && <LevelUpModal />}
+            {children}
+        </ChallengesContext.Provider>
+    );
 }
